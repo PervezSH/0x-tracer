ac97ddb50b0759148e44548d61936e2d2ba83ed5
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-06-08 17:50:09 +0530

    build(bootstrap): add bootstrap & sass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import '@styles/globals.css';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import '@styles/scss/global.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
