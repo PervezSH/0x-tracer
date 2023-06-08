49ebb8e8272eae453a443fb5aff500ede56ec2f8
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-06-08 15:55:29 +0530

    feat(init): initialize next-app 🎉

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css';
+import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '0x-Tracer',
+  description: 'The Web3 Portfolio Tracer',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
