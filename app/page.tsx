3f932ba3c5b08d421d9ea5b8c18654be595cdc81
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-06-11 11:40:43 +0530

    reafactor: search component 🎨

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-'use client';
-import Image from 'next/image';
-import { useRef, FormEvent, useState } from 'react';
-import { isAddress } from 'ethers';
-import { useRouter } from 'next/navigation';
+import { FC } from 'react';
+import { Search } from '@components';
 
-const Home: React.FC = () => {
-  const router = useRouter();
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const [inputValue, setInputValue] = useState<string>('');
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const inputVal = inputRef.current?.value;
-    if (!inputVal) return;
-    if (!isAddress(inputVal)) return;
-    router.push(`/${inputVal}`);
-  };
-
+const Home: FC = () => {
   return (
     <main className="d-flex flex-column mt-5 mb-3 gap-3">
       <article>
@@ -31,38 +15,7 @@ const Home: React.FC = () => {
           </span>
         </p>
       </article>
-      <form
-        className="d-flex bg-primary rounded-4 px-4 py-1 w-75"
-        style={{ boxShadow: ' 0px 10px 20px #B1E6FF' }}
-        onSubmit={handleSubmit}
-      >
-        <input
-          id="search-panel"
-          ref={inputRef}
-          type="text"
-          className="form-control border-0"
-          autoComplete="off"
-          spellCheck="false"
-          placeholder="Search Address"
-          maxLength={42}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button className="btn btn-primary" type="submit">
-          <Image
-            src={
-              inputValue === ''
-                ? '/assets/icons/search.svg'
-                : isAddress(inputValue)
-                ? '/assets/icons/check.svg'
-                : '/assets/icons/warning.svg'
-            }
-            alt={'search'}
-            width={24}
-            height={24}
-          />
-        </button>
-      </form>
+      <Search />
     </main>
   );
 };
