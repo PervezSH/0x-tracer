fbfad96677ad1ad2db83e2aa84eff6fc24d1dcad
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-06-18 12:12:50 +0530

    feat(profile): retrie & display ens informations ✨

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,14 +1,35 @@
-import { FC } from 'react';
+import { FC, cache } from 'react';
 import Image from 'next/image';
+import { ethers } from 'ethers';
 
 import NetCurve from './NetCurve';
 import { usdValue24h } from '@utils';
+
+const getEns = cache(async (address: string) => {
+  try {
+    const provider = new ethers.JsonRpcProvider('https://rpc.ankr.com/eth');
+    const name = await provider.lookupAddress(address);
+    const avatar = await provider.getAvatar(name!);
+    return {
+      name,
+      avatar,
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      name: null,
+      avatar: null,
+    };
+  }
+});
 
 interface ProfileProps {
   address: string;
 }
 
-const Profile: FC<ProfileProps> = ({ address }) => {
+const Profile: FC<ProfileProps> = async ({ address }) => {
+  const ens = await getEns(address);
+
   return (
     <div className="d-flex flex-column flex-lg-row  gap-3 mt-4 align-items-center">
       <div>
@@ -19,15 +40,17 @@ const Profile: FC<ProfileProps> = ({ address }) => {
           height={96}
           style={{ position: 'absolute' }}
         />
-        <Image
-          src={'/assets/images/boredApe.webp'}
-          alt={'avatar'}
-          width={96}
-          height={96}
-        />
+        {ens.avatar ? (
+          <img src={ens.avatar} alt={'ens-avatar'} width={96} height={96} />
+        ) : (
+          <div
+            className="bg-primary"
+            style={{ width: '96px', height: '96px' }}
+          ></div>
+        )}
       </div>
       <section className="d-flex flex-column justify-content-center align-items-center align-items-lg-start">
-        <h6 className="fw-semibold m-0">{`0x_per.eth`}</h6>
+        {ens.name && <h6 className="fw-semibold m-0">{ens.name}</h6>}
         <div className="d-flex flex-column flex-lg-row align-items-center gap-2">
           <p className="fs-6 text-secondary m-0">{address}</p>
           <span className="badge d-flex align-items-center gap-1 bg-badge-bg text-primary py-2 px-3 rounded-4">
