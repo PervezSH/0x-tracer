7c98c5930aa633bf301f1ff2168c4db519bc6265
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-06-19 21:49:35 +0530

    fix: module not found 🐛

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@
 const path = require('path');
 
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
+    return config;
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
